753aaa11e46a15787c5a4d556041e366efa262a6
sandpinedo <1440690@senati.pe>
2023-09-14 16:34:27 -0500

    Modifique el acoplamiento de SQL con visual.net

---
+++ ProyectoLogin/Models/DbpruebaContext.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace ProyectoLogin.Models;
+
+public partial class DbpruebaContext : DbContext
+{
+    public DbpruebaContext()
+    {
+    }
+
+    public DbpruebaContext(DbContextOptions<DbpruebaContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Usuario> Usuarios { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Server=(local); DataBase=DBPRUEBA; Trusted_Connection=True; TrustServerCertificate=True;");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Usuario>(entity =>
+        {
+            entity.HasKey(e => e.IdUsuario).HasName("PK__USUARIO__5B65BF97B62D1E58");
+
+            entity.ToTable("USUARIO");
+
+            entity.Property(e => e.Clave)
+                .HasMaxLength(100)
+                .IsUnicode(false)
+                .HasColumnName("clave");
+            entity.Property(e => e.Correo)
+                .HasMaxLength(50)
+                .IsUnicode(false)
+                .HasColumnName("correo");
+            entity.Property(e => e.NombreUsuario)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
